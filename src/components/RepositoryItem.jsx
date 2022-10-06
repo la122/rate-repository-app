a7a6cf34d73cdd76c0e7d514c52b2f463924b3b8
la122 <70019096+la122@users.noreply.github.com>
2022-10-06 18:04:23 +0200

    10.3 done

---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,15 @@
+import { View, Text } from "react-native";
+
+const FlatListItem = ({ item }) => (
+  <View>
+    <Text>Full name: {item.fullName}</Text>
+    <Text>Description: {item.description}</Text>
+    <Text>Language: {item.language}</Text>
+    <Text>Forks: {item.language}</Text>
+    <Text>Stars: {item.stargazersCount}</Text>
+    <Text>Ratings: {item.ratingAverage}</Text>
+    <Text>Reviews: {item.reviewCount}</Text>
+  </View>
+);
+
+export default FlatListItem;
