6f0b46b14f0f0b93c8853e8dc1b2837136b2234e
la122 <70019096+la122@users.noreply.github.com>
2022-10-07 16:50:08 +0200

    10.7 done

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,4 +1,4 @@
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, ScrollView } from "react-native";
 import Constants from "expo-constants";
 import AppBarTab from "./AppBarTab";
 import theme from "../theme";
@@ -15,8 +15,10 @@ const styles = StyleSheet.create({
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <AppBarTab to="/">Repositories</AppBarTab>
-      <AppBarTab to="/signIn">Sign in</AppBarTab>
+      <ScrollView horizontal>
+        <AppBarTab to="/">Repositories</AppBarTab>
+        <AppBarTab to="/signIn">Sign in</AppBarTab>
+      </ScrollView>
     </View>
   );
 };
